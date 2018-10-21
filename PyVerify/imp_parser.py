e3926fcdc04043de61899039911dd39db3b9a0b5
zacsimile <zachcm@gmail.com>
2018-10-21 17:15:37 -0400

    Fixed IMP to GC (I think).

--- PyVerify/imp_parser.py
+++ PyVerify/imp_parser.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from lark import Lark, Transformer, Tree
 from lark.lexer import Token
 
@@ -27,7 +28,7 @@ class ImpParser:
 
         # Check that we have an IMP file
         if not (isinstance(fn, str) and fn.split('.')[1] == 'imp'):
-            raise ValueError("File must be .imp.")
+            raise ValueError('File must be .imp.')
 
         # Read the file data in
         stream = open(fn, 'r')
@@ -155,30 +156,138 @@ class ImpToGC(Transformer):
     #     else:
     #         return []
 
-    def program(self, p):
-        print('---program')
-        print(p)
-        print(type(p))
-        print('----program')
+    # def program(self, p):
+    #     print('---program')
+    #     print(p)
+    #     print(type(p))
+    #     print('----program')
+    #
+    # def lt(self, p):
+    #     print('---lt')
+    #     print(p)
+    #     print(type(p))
+    #     print('---lt')
+    #
+    # def neg(self, p):
+    #     print('---neg')
+    #     print(p)
+    #     print(type(p))
+    #     print('---neg')
 
-    def lt(self, p):
-        print('---lt')
-        print(p)
-        print(type(p))
-        print('---lt')
+    def assemble_pre(self, l):
+        pre = []
+        for t in l:
+            if type(t) == Tree and t.data == 'pre':
+                pre.extend(t.children)
+        if len(pre) > 1:
+            return ' && '.join(str(t).strip('[]') for t in pre)
+        else:
+            return ''.join(str(t).strip('[]') for t in pre)
 
-    def neg(self, p):
-        print('---neg')
-        print(p)
-        print(type(p))
-        print('---neg')
+    def assemble_post(self, l):
+        post = []
+        for t in l:
+            if type(t) == Tree and t.data == 'post':
+                post.extend(t.children)
+        if len(post) > 1:
+            return ' && '.join(str(t).strip('[]') for t in post)
+        else:
+            return ''.join(str(t).strip('[]') for t in post)
+
+    def assemble_invariants(self, l):
+        inv = []
+        for t in l:
+            if type(t) == Tree and t.data == 'inv':
+                inv.extend(t.children)
+        if len(inv) > 1:
+            return ' && '.join(str(t).strip('[]') for t in inv)
+        else:
+            return ''.join(str(t).strip('[]') for t in inv)
+
+    def assemble_havoc(self, l):
+        h = []
+        for t in l:
+            if type(t) == str:
+                h.extend(re.findall(re.compile(r'havoc [A-Za-z|_|\d+(\.\d*)?]+;'), t))
+        return ' '.join(str(s) for s in h)
+
+    def eq(self, c):
+        return ' = '.join(str(a) for a in c)
+
+    def neq(self, c):
+        return ' != '.join(str(a) for a in c)
+
+    def leq(self, c):
+        return ' <= '.join(str(a) for a in c)
+
+    def geq(self, c):
+        return ' >= '.join(str(a) for a in c)
+
+    def lt(self, c):
+        return ' < '.join(str(a) for a in c)
+
+    def gt(self, c):
+        return ' > '.join(str(a) for a in c)
+
+    def add(self, m):
+        return ' + '.join(str(a) for a in m)
+
+    def sub(self, m):
+        return ' - '.join(str(a) for a in m)
+
+    def mult(self, m):
+        return ' * '.join(str(a) for a in m)
+
+    def div(self, m):
+        return ' / '.join(str(a) for a in m)
+
+    def mod(self, m):
+        return ' % '.join(str(a) for a in m)
+
+    def implies(self, i):
+        return ' ==> '.join(str(a) for a in i)
+
+    def forall(self, f):
+        return 'forall ' + ' '.join(str(a) for a in f[:-1]) + ', ' + str(f[-1])
+
+    def exists(self, f):
+        return 'exists ' + ' '.join(str(a) for a in f[:-1]) + ', ' + str(f[-1])
+
+    def read(self, a):
+        return 'read(' + ', '.join(str(e) for e in a) + ')'
+
+    def block(self, b):
+        return ' '.join(str(e) for e in b).strip('[]')
+
+    def assign(self, a):
+        self.tmpcount += 1
+        varstring = 'tmp_' + str(self.tmpcount)
+        replacestring = str(a[1]).replace(str(a[0]), varstring)
+        try:
+            if a[1].type == Token.NUMBER:
+                replacestring = str(a[1])
+        except AttributeError:
+            pass
+        return 'assume ' + varstring + ' = ' + str(a[0]) + '; havoc ' + str(a[0]) + '; assume ' + str(a[0]) + ' = ' + replacestring + ';'
+
+    def write(self, a):
+        return str(a[0]) + ' = write(' + ', '.join(str(e) for e in a) + ')'
+
+    def _not(self, b):
+        return "!(" + str(b) + ")"
 
     def whilestmt(self, w):
-        print('---while')
-        print(w)
-        print(type(w))
-        print('---endwhile')
-        # out = []
+        # print('---while')
+        # print(w)
+        # print(type(w))
+        # print(self.assemble_invariants(w))
+        # print(self.assemble_havoc(w))
+        # print('---endwhile')
+        out = []
+        out.extend(['assert ' + self.assemble_invariants(w) + '; '])
+        out.extend([self.assemble_havoc(w) + ' '])
+        out.extend(['assume ' + self.assemble_invariants(w) + '; '])
+        out.extend(['(assume ' + str(w[0]) + '; ' + str(w[-1]) + ' assert ' + self.assemble_invariants(w) + '; assume false;) ||| ' + 'assume ' + self._not(w[0]) + ';'])
         # # Assert/assume invariant
         # inv = self.construct_invariant(w)
         # out.append(Tree('assert', inv))
@@ -191,14 +300,48 @@ class ImpToGC(Transformer):
         #     out.append(Tree('choice', [[Tree('assume', w[0]), w[1], Tree('assert', inv), Tree('assume', 'false')], Tree('assume', self._not(w[0]))]))
         # else:
         #     out.append(Tree('choice', [[Tree('assume', w[0]), w[1], Tree('assume', 'false')], Tree('assume', self._not(w[0]))]))
-        # return out
+        return ''.join(str(o) for o in out)
+
+    def ifstmt(self, i):
+        out = []
+        out.extend(['assume ' + str(i[0]) + '; '])
+        out.extend([str(i[1]) + ' '])
+        if len(i) > 2:
+            out.extend(['||| assume ' + str(i[0]) + '; '])
+            out.extend([str(i[2]) + ' '])
+        return ''.join(str(o) for o in out)
+
+    def program(self, p):
+        out = []
+        out.extend(['assume ' + self.assemble_pre(p) + '; '])
+        out.extend(str(p[-1]) + ' ')
+        out.extend(['assert ' + self.assemble_post(p) + '; '])
+        return ''.join(str(o) for o in out)
+
+    def band(self, b):
+        return ' && '.join(str(e) for e in b)
+
+    def bor(self, b):
+        return ' || '.join(str(e) for e in b)
+
+    def bnot(self, b):
+        return '!' + ''.join(str(e) for e in b)
+
+    def bparen(self, b):
+        return '(' + ''.join(str(e) for e in b) + ')'
+
+    def neg(self, n):
+        return self.sub([0, n[0]])
+
+    def parassign(self, n):
+        return self.assign([n[0], n[2]]) + ' ' + self.assign([n[1], n[3]])
 
 if __name__ == '__main__':
     parser = ImpParser()
     tree = parser.parse_file(sys.argv[1])
     try:
-        print(tree)
-        #ImpToGC().transform(tree)
+        #print(tree.pretty())
+        print(ImpToGC().transform(tree))
     except AttributeError:
         print('yes')
         #print(tree)
