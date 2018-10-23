d670f59f9763979bb856ec86e3cb70424921eb9f
zacsimile <zachcm@gmail.com>
2018-10-22 23:55:31 -0400

    Rewrote IMP to GC as a tree.

--- PyVerify/imp_parser.py
+++ PyVerify/imp_parser.py
@@ -2,6 +2,8 @@ import sys
 import re
 from lark import Lark, Transformer, Tree
 from lark.lexer import Token
+from itertools import chain
+
 
 
 class ImpParser:
@@ -64,284 +66,273 @@ class ImpToGC(Transformer):
         Transformer.__init__(self)
         self.tmpcount = -1
 
-    # def _not(self, b):
-    #     if type(b) == list:
-    #         out = []
-    #         for t in b:
-    #             out.append(self._not(t))
-    #         return out
-    #
-    #     if b.data == 'eq':
-    #         return Tree('neq', b.children)
-    #     if b.data == 'neq':
-    #         return Tree('eq', b.children)
-    #     if b.data == 'geq':
-    #         return Tree('lt', b.children)
-    #     if b.data == 'lt':
-    #         return Tree('geq', b.children)
-    #     if b.data == 'leq':
-    #         return Tree('gt', b.children)
-    #     if b.data == 'gt':
-    #         return Tree('leq', b.children)
-    #     if b.data == 'or':
-    #         return Tree('and', self._not(b.children))
-    #     if b.data == 'and':
-    #         return Tree('or', self._not(b.children))
-    #     if b.data == 'not':
-    #         return b.children
-    #     else:
-    #         return Tree('not', b.children)
-    #
-    # def block(self, b):
-    #     return b
-    #
-    # def program(self, p):
-    #     if len(p) > 2 and p[2].data == 'assert':
-    #         return [p[0].value, p[1], p[3:], p[2]]
-    #     else:
-    #         return [p[0].value, p[1:]]
-    #
-    # def assign(self, a):
-    #     self.tmpcount += 1
-    #     varstring = 'tmp_' + str(self.tmpcount)
-    #     return [Tree('assume', Tree('eq', [Token('NAME', varstring), a[0]])), Tree('havoc', a[0]),
-    #             Tree('assume', Tree('eq', [a[0], Tree('replace', [a[1], Token('NAME', varstring), a[0]])]))]
-    #
-    # def write(self, a):
-    #     self.tmpcount += 1
-    #     varstring = 'tmp_' + str(self.tmpcount)
-    #     return [Tree('assume', Tree('eq', [Token('NAME', varstring), a[0]])), Tree('havoc', a[0]),
-    #             Tree('assume', Tree('eq', [a[0], Tree('write', ['tmp_' + str(self.tmpcount), a[1], a[2]])]))]
-    #
-    # def pre(self, p):
-    #     return Tree('assume', p)
-    #
-    # def post(self, p):
-    #     return Tree('assert', p)
-    #
-    # def parassign(self, p):
-    #     return [self.assign([p[0], p[2]]), self.assign([p[1], p[3]])]
-    #
-    # def ifstmt(self, b):
-    #     if len(b) == 2:
-    #         return [Tree('assume', b[0]), b[1]]
-    #     if len(b) == 3:
-    #         return Tree('choice', [Tree('assume', b[0]), b[1]], [Tree('assume', self._not(b[0])), b[2]])
-    #
-    # def neg(self, n):
-    #     return Tree('sub', [Token('NUMBER', 0), n])
-    #
-    # def find_tree_data(self, data, l):
-    #     out = []
-    #     if type(l) == list:
-    #         for c in l:
-    #             if type(c) == Tree:
-    #                 if c.data == data:
-    #                     out.append(c.children)
-    #     if type(l) == Tree:
-    #         if c.data == data:
-    #             out.append(c.children)
-    #     return out
-    #
-    # def construct_invariant(self, l):
-    #     inv = self.find_tree_data('inv', l)
-    #     if len(inv) > 0:
-    #         i_and = []
-    #         for i in inv:
-    #             i_and.append(i)
-    #         if len(i_and) > 1:
-    #             return Tree('and', i_and)
-    #         else:
-    #             return i_and
-    #     else:
-    #         return []
+    def _not(self, b):
+        """
+        Negate a boolean.
+        """
+        if type(b) == list:
+            out = []
+            for t in b:
+                out.append(self._not(t))
+            return out
 
-    # def program(self, p):
-    #     print('---program')
-    #     print(p)
-    #     print(type(p))
-    #     print('----program')
-    #
-    # def lt(self, p):
-    #     print('---lt')
-    #     print(p)
-    #     print(type(p))
-    #     print('---lt')
-    #
-    # def neg(self, p):
-    #     print('---neg')
-    #     print(p)
-    #     print(type(p))
-    #     print('---neg')
+        if b.data == 'bparen':
+            return self._not(b.children)
+        if b.data == 'eq':
+            return Tree('neq', b.children)
+        if b.data == 'neq':
+            return Tree('eq', b.children)
+        if b.data == 'geq':
+            return Tree('lt', b.children)
+        if b.data == 'lt':
+            return Tree('geq', b.children)
+        if b.data == 'leq':
+            return Tree('gt', b.children)
+        if b.data == 'gt':
+            return Tree('leq', b.children)
+        if b.data == 'bor':
+            return Tree('band', self._not(b.children))
+        if b.data == 'band':
+            return Tree('bor', self._not(b.children))
+        if b.data == 'not':
+            return b.children
+        else:
+            # Do not fail silently
+            print(b)
+            raise NotImplemented
 
     def assemble_pre(self, l):
+        """
+        Group preconditions.
+        """
         pre = []
         for t in l:
             if type(t) == Tree and t.data == 'pre':
                 pre.extend(t.children)
         if len(pre) > 1:
-            return ' && '.join(str(t).strip('[]') for t in pre)
-        else:
-            return ''.join(str(t).strip('[]') for t in pre)
+            previous = pre.pop()
+            while len(pre) > 0:
+                current = pre.pop()
+                previous = Tree('band', [current, previous])
+            return previous
+        return pre
 
     def assemble_post(self, l):
+        """
+        Group postconditions.
+        """
         post = []
         for t in l:
             if type(t) == Tree and t.data == 'post':
                 post.extend(t.children)
         if len(post) > 1:
-            return ' && '.join(str(t).strip('[]') for t in post)
-        else:
-            return ''.join(str(t).strip('[]') for t in post)
+            postvious = post.pop()
+            while len(post) > 0:
+                current = post.pop()
+                postvious = Tree('band', [current, postvious])
+            return postvious
+        return post
 
     def assemble_invariants(self, l):
+        """
+        Group invariants.
+        """
         inv = []
         for t in l:
             if type(t) == Tree and t.data == 'inv':
                 inv.extend(t.children)
         if len(inv) > 1:
-            return ' && '.join(str(t).strip('[]') for t in inv)
-        else:
-            return ''.join(str(t).strip('[]') for t in inv)
+            invious = inv.pop()
+            while len(inv) > 0:
+                current = inv.pop()
+                invious = Tree('band', [current, invious])
+            return invious
+        return inv
 
     def assemble_havoc(self, l):
-        h = []
+        """
+        Group havoc.
+        """
+        hav = []
         for t in l:
-            if type(t) == str:
-                h.extend(re.findall(re.compile(r'havoc [A-Za-z|_|\d+(\.\d*)?]+;'), t))
-        return ' '.join(str(s) for s in h)
+            if type(t) == Tree:
+                if t.data == 'havoc':
+                    hav.extend([Tree('havoc', t.children)])
+                else:
+                    hav.extend(self.assemble_havoc([t.children]))
+            if type(t) == list:
+                hav.extend(self.assemble_havoc(t))
+        return hav
 
-    def eq(self, c):
-        return ' = '.join(str(a) for a in c)
+    def replacetree(self, t, find_token, replace_token):
+        """
+        Recursively iterate through tree t and replace find_token with replace_token. Used for replacement rule.
 
-    def neq(self, c):
-        return ' != '.join(str(a) for a in c)
+        Parameters
+        ----------
+        t : Tree
+            A tree over which to iterate.
+        find_token : Token
+            Variable to find.
+        replace_token : str
+            Replacement variable.
 
-    def leq(self, c):
-        return ' <= '.join(str(a) for a in c)
-
-    def geq(self, c):
-        return ' >= '.join(str(a) for a in c)
-
-    def lt(self, c):
-        return ' < '.join(str(a) for a in c)
-
-    def gt(self, c):
-        return ' > '.join(str(a) for a in c)
-
-    def add(self, m):
-        return ' + '.join(str(a) for a in m)
-
-    def sub(self, m):
-        return ' - '.join(str(a) for a in m)
-
-    def mult(self, m):
-        return ' * '.join(str(a) for a in m)
-
-    def div(self, m):
-        return ' / '.join(str(a) for a in m)
-
-    def mod(self, m):
-        return ' % '.join(str(a) for a in m)
-
-    def implies(self, i):
-        return ' ==> '.join(str(a) for a in i)
-
-    def forall(self, f):
-        return 'forall ' + ' '.join(str(a) for a in f[:-1]) + ', ' + str(f[-1])
-
-    def exists(self, f):
-        return 'exists ' + ' '.join(str(a) for a in f[:-1]) + ', ' + str(f[-1])
-
-    def read(self, a):
-        return 'read(' + ', '.join(str(e) for e in a) + ')'
-
-    def block(self, b):
-        return ' '.join(str(e) for e in b).strip('[]')
+        Returns
+        -------
+        Tree
+            Tree with replaced token.
+        """
+        out = []
+        for v in t.children:
+            if type(v) == Token:
+                if v.value == find_token.value:
+                    out.extend([Token('NAME', replace_token)])
+                else:
+                    out.extend([Token(v.type, v.value)])
+            if type(v) == Tree:
+                out.extend([Tree(v.data, self.replacetree(v, find_token, replace_token))])
+        return out
 
     def assign(self, a):
+        """
+        Rule for parsing ?assign: NAME ":=" aexp ";" to guarded command.
+
+        Parameters
+        ----------
+        a : list
+           [Token.NAME, Tree('aexp', _)]
+
+        Returns
+        -------
+        List of guarded commands.
+        """
         self.tmpcount += 1
         varstring = 'tmp_' + str(self.tmpcount)
-        replacestring = str(a[1]).replace(str(a[0]), varstring)
-        try:
-            if a[1].type == Token.NUMBER:
-                replacestring = str(a[1])
-        except AttributeError:
-            pass
-        return 'assume ' + varstring + ' = ' + str(a[0]) + '; havoc ' + str(a[0]) + '; assume ' + str(a[0]) + ' = ' + replacestring + ';'
+        out = []
+        out.append(Tree('assume', Tree('eq', [Token('NAME', varstring), Token(a[0].type, a[0].value)])))
+        out.append(Tree('havoc', Token(a[0].type, a[0].value)))
+        if type(a[1]) == Tree:
+            replacetree = Tree(a[1].data, self.replacetree(a[1], a[0], varstring))
+            out.append(Tree('assume', Tree('eq', [Token(a[0].type, a[0].value), replacetree])))
+        else:
+            try:
+                if a[0].value != a[1].value:
+                    out.append(Tree('assume', Tree('eq', [Token(a[0].type, a[0].value), Token(a[1].type, a[1].value)])))
+                if a[0].value == a[1].value:
+                    out.append(Tree('assume', Tree('eq', [Token(a[0].type, a[0].value), Token('NAME', varstring)])))
+            except AttributeError:
+                # Don't fail silently
+                print(a)
+                raise
+        return out
+
+    def block(self, b):
+        """
+        Returns a tree block. Had to use this to "in place" clear out extra brackets generated from assign, etc.
+        """
+        out = []
+        for v in b:
+            if isinstance(type(v), type(None)):
+                continue
+            if type(v) == list:
+                for a in v:
+                    out.append(a)
+            else:
+                out.append(v)
+        return Tree('block', out)
 
     def write(self, a):
-        return str(a[0]) + ' = write(' + ', '.join(str(e) for e in a) + ')'
-
-    def _not(self, b):
-        return "!(" + str(b) + ")"
+        """
+        Convert array write to a guaraded command.
+        """
+        return self.assign([a[0], Tree('write', a)])
 
     def whilestmt(self, w):
-        # print('---while')
-        # print(w)
-        # print(type(w))
-        # print(self.assemble_invariants(w))
-        # print(self.assemble_havoc(w))
-        # print('---endwhile')
+        """
+        Convert while statement to guarded command.
+        """
+        invs = self.assemble_invariants(w)
+        b = [Tree('assert', invs), self.assemble_havoc(w), Tree('assume', invs),
+             Tree('wpor', [Tree('block', [Tree('assume', w[0]), w[-1], Tree('assert', invs), Tree('assume', False)]),
+                   Tree('assume', self._not(w[0]))])]
         out = []
-        out.extend(['assert ' + self.assemble_invariants(w) + '; '])
-        out.extend([self.assemble_havoc(w) + ' '])
-        out.extend(['assume ' + self.assemble_invariants(w) + '; '])
-        out.extend(['(assume ' + str(w[0]) + '; ' + str(w[-1]) + ' assert ' + self.assemble_invariants(w) + '; assume false;) ||| ' + 'assume ' + self._not(w[0]) + ';'])
-        # # Assert/assume invariant
-        # inv = self.construct_invariant(w)
-        # out.append(Tree('assert', inv))
-        # # Havoc variables
-        # havoc = self.find_tree_data('havoc', w)
-        # for h in havoc:
-        #     out.append(Tree('havoc', h))
-        # if type(inv) == Tree:
-        #     out.append(Tree('assume', inv))
-        #     out.append(Tree('choice', [[Tree('assume', w[0]), w[1], Tree('assert', inv), Tree('assume', 'false')], Tree('assume', self._not(w[0]))]))
-        # else:
-        #     out.append(Tree('choice', [[Tree('assume', w[0]), w[1], Tree('assume', 'false')], Tree('assume', self._not(w[0]))]))
-        return ''.join(str(o) for o in out)
+        for v in b:
+            if type(v) == list:
+                for a in v:
+                    out.append(a)
+            else:
+                out.append(v)
+        return out
 
     def ifstmt(self, i):
+        b = [Tree('assume', i[0]), i[1]]
         out = []
-        out.extend(['assume ' + str(i[0]) + '; '])
-        out.extend([str(i[1]) + ' '])
+        for v in b:
+            if type(v) == list:
+                for a in v:
+                    out.append(a)
+            else:
+                out.append(v)
         if len(i) > 2:
-            out.extend(['||| assume ' + str(i[0]) + '; '])
-            out.extend([str(i[2]) + ' '])
-        return ''.join(str(o) for o in out)
+            out = Tree('wpor', [Tree('block', [Tree('assume', i[0]), i[1]]),
+                              Tree('block', [Tree('assume', self._not(i[0])), i[2]])])
+        return out
 
     def program(self, p):
+        """
+        Assemble program in the correct order.
+        """
+        b = [[Token(p[0].type, p[0].value)], self.assemble_pre(p), p[-1], self.assemble_post(p)]
         out = []
-        out.extend(['assume ' + self.assemble_pre(p) + '; '])
-        out.extend(str(p[-1]) + ' ')
-        out.extend(['assert ' + self.assemble_post(p) + '; '])
-        return ''.join(str(o) for o in out)
+        for v in b:
+            if type(v) == list:
+                for a in v:
+                    out.append(a)
+            else:
+                out.append(v)
+        return Tree('program', out)
 
-    def band(self, b):
-        return ' && '.join(str(e) for e in b)
-
-    def bor(self, b):
-        return ' || '.join(str(e) for e in b)
-
-    def bnot(self, b):
-        return '!' + ''.join(str(e) for e in b)
-
-    def bparen(self, b):
-        return '(' + ''.join(str(e) for e in b) + ')'
+    def parassign(self, p):
+        """
+        Split pairwise assignment into two assign statements.
+        """
+        out = []
+        out.extend(self.assign([p[0], p[2]]))
+        out.extend(self.assign([p[1], p[3]]))
+        return out
 
     def neg(self, n):
-        return self.sub([0, n[0]])
-
-    def parassign(self, n):
-        return self.assign([n[0], n[2]]) + ' ' + self.assign([n[1], n[3]])
+        return Tree('sub', [Token('NUMBER', 0), n[0]])
 
 if __name__ == '__main__':
-    parser = ImpParser()
-    tree = parser.parse_file(sys.argv[1])
+    imp_parser = ImpParser()
+    tree = imp_parser.parse_file(sys.argv[1])
+    print(str(tree) + '\n')
     try:
         #print(tree.pretty())
-        print(ImpToGC().transform(tree))
+        gc = ImpToGC().transform(tree)
+        print(str(gc) + '\n')
+        # vc = gc + ', true'
+        # vc_list = vc.split(';')
+        # vc_prev = vc_list.pop().strip()
+        # vc_nested = 'wp(' + vc_prev + ')'
+        # while len(vc_list) > 0:
+        #     vc_new = vc_list.pop().strip()
+        #     vc_anded = vc_new.split('|||')
+        #     if len(vc_anded) > 1:
+        #         res = vc_prev.split(',')[-1]
+        #         vc_nested = 'wp(' + vc_anded[0] + ', ' + res + ') && ' + vc_nested
+        #     else:
+        #         vc_nested = 'wp(' + vc_new + ', ' + vc_nested + ')'
+        #     vc_prev = vc_new
+        # print(vc_nested)
+
     except AttributeError:
-        print('yes')
+        raise
+        #print('imp attribute error')
         #print(tree)
+    # vc_parser = Lark.open('vc.lark', parser='earley', lexer='standard')
+    # try:
+    #     print(vc_parser.parse(vc_nested).pretty())
+    # except AttributeError:
+    #     print('vc attribute error')
